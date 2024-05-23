69df9616bb68060a0b2d4ed19a8315194c822e11
Mitchell Mudd <developer@mitchellmudd.dev>
2024-05-23 16:00:44 -0400

    Got navigation working much better

---
+++ mobile/scrapyard/app/(tabs)/index.tsx
@@ -0,0 +1,135 @@
+import { Image, StyleSheet, Text, StatusBar, TouchableOpacity, Pressable } from 'react-native';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { Link } from 'expo-router';
+
+const walls = [
+  [
+    {
+      title: 'slab',
+      id: 'slab'
+    },
+    {
+      title: 'cone',
+      id: 'cone'
+    },
+  ],
+  [
+    {
+      title: '35',
+      id: '35&deg;'
+    },
+    {
+      title: 'cave',
+      id: 'cave'
+    }
+  ],
+  [
+    {
+      title: 'top rope',
+      id: 'top-rope'
+    },
+    {
+      title: 'route of the week',
+      id: 'rotw'
+    }
+  ],
+  [
+    {
+      title: 'cove',
+      id: 'cove'
+    }
+  ]
+]
+
+type ItemProps = { title: string, id: string };
+
+const handlePress = (id: string) => {
+
+}
+
+const Item = ({ title, id }: ItemProps) => (
+  <Link push={true} href={{
+    pathname: `wall/[id]`,
+    params: { id: id, title: title }
+  }} asChild>
+    <TouchableOpacity style={styles.item} onPress={() => handlePress(id)}>
+      <Text style={styles.title}>{title}</Text>
+    </TouchableOpacity>
+  </Link>
+);
+
+export default function HomeScreen() {
+  return (
+    // TODO: replace with scrapyard logo
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerImage={
+        <Image
+          source={require('@/assets/images/partial-react-logo.png')}
+          style={styles.reactLogo}
+        />
+      }>
+      <ThemedView style={styles.wallsContainer}>
+
+        <Link href={'search'} asChild>
+          <Pressable>
+            <ThemedText>Search</ThemedText>
+          </Pressable>
+        </Link>
+
+        {walls.map((wall, key) => {
+          return (
+            <ThemedView key={key} style={styles.wallContainer}>
+              <Item title={wall[0].title} id={wall[0].id} />
+
+              {wall[1] && <Item title={wall[1].title} id={wall[1].id} />}
+            </ThemedView>
+          )
+        })}
+      </ThemedView>
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  wallsContainer: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 40,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+  wallContainer: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%'
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    minHeight: 150,
+    width: '40%',
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 32,
+    textAlign: 'center'
+  },
+});
---
+++ mobile/scrapyard/app/_layout.tsx
@@ -0,0 +1,45 @@
+import { DarkTheme, DefaultTheme, NavigationContainer, Route, ThemeProvider, useRoute } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { Stack, useLocalSearchParams } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+import 'react-native-reanimated';
+
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const params = useLocalSearchParams();
+  let idParam = params.id;
+
+  // if (typeof idParam !== 'string') {
+  //   throw new Error('idParam must be a string');
+  // }
+
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+        <Stack.Screen name="wall/[id]" options={{ title: 'wall' }} />
+      </Stack>
+    </ThemeProvider>
+  );
+}
